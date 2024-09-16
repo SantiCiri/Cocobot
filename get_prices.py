5ac59be807345fe66b06e186252de997d31f5de4
Santiago <cirigliano.santiago@gmail.com>
2024-09-16 19:06:38 -0300

    implementacion de pase, contrapase y 38/41

--- get_prices.py
+++ get_prices.py
@@ -1,3 +1,14 @@
 from trading_helper import TradingHelper
+
+import time
+from datetime import datetime
+
+end_time = datetime.now().replace(hour=10, minute=58, second=0)
+if datetime.now() > end_time:
+    end_time = end_time.replace(day=end_time.day + 1)
+
+while datetime.now() < end_time:
+    time.sleep(60)
+
 app=TradingHelper()
 app.get_cocos_prices(mail="cirigliano.santiago@gmail.com")
